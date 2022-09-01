77acb26b266ecb4460a604cd52e75d590a06a06c
Carlamejias <102421609+Carlamejias@users.noreply.github.com>
2022-09-01 13:35:18 -0400

    Add files via upload

---
+++ src/app/especialidad/especialidad.page.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-especialidad',
+  templateUrl: './especialidad.page.html',
+  styleUrls: ['./especialidad.page.scss'],
+})
+export class EspecialidadPage implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+  
+  presencial(){
+
+    this.router.navigate(['/login']);
+  }
+
+  telemedicina(){
+
+    this.router.navigate(['/login']);
+  }
+  dental(){
+
+    this.router.navigate(['/login']);
+  }
+
+}
+
